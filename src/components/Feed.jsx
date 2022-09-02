fd725f56e0a368b3460a936f1b10c0ddb62678bd
Puspal Tarafdar <puspaltarafdar707@gmail.com>
2022-09-02 10:43:00 +0530

    fourth update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import {Box, Stack, Typography} from '@mui/material';
-import Sidebar from './Sidebar';
+import {Sidebar, Videos} from './';
+
 
 
 const Feed = () => {
@@ -15,8 +16,20 @@ const Feed = () => {
         variant = "body2" sx={{mt:1.5, color: '#fff'}}>
           Copyright 2022 Youtube Clone
         </Typography>
+        </Box>
+          <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+            <Typography vaiant="h4"
+            fontWeight="bold" mb={2} sx={{
+              color: 'white'
+            }}>
+              New <span style={{color: '#F31503'}}>
+                videos
+              </span>
+            </Typography>
+            <Videos videos = {[]}/>
+          
+        </Box>
 
-      </Box>
 
     </Stack>
   )
