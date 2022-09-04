34a8fcb62232e7ee2e0bbef866d5946d9675b305
Puspal Tarafdar <puspaltarafdar707@gmail.com>
2022-09-04 18:12:07 +0530

    fifth update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,17 +1,29 @@
 import {useState, useEffect} from 'react';
 import {Box, Stack, Typography} from '@mui/material';
+
+import {fetchFromAPI} from '../utils/fetchFromAPI';
 import {Sidebar, Videos} from './';
 
 
 
 const Feed = () => {
+
+  const [selectedCategory, setselectedCategory] = useState('New');
+
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  },[selectedCategory])
+
   return (
     <Stack sx={{ flexDirection: {
       sx: "column", md: "row"
     }}}>
       <Box sx={{height: {sx: 'auto', md: '92vh'},
       borderRight: '1px solid #3d3d3d', px: {sx:0, md:2}}}>
-        <Sidebar/>
+        <Sidebar
+        selectedCategory={selectedCategory}
+        setselectedCategory={setselectedCategory}
+        />
         <Typography className="copyright"
         variant = "body2" sx={{mt:1.5, color: '#fff'}}>
           Copyright 2022 Youtube Clone
@@ -22,7 +34,7 @@ const Feed = () => {
             fontWeight="bold" mb={2} sx={{
               color: 'white'
             }}>
-              New <span style={{color: '#F31503'}}>
+              {selectedCategory} <span style={{color: '#F31503'}}>
                 videos
               </span>
             </Typography>
